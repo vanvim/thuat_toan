0426761ab5d32b44ac75272d006e9567b8286082
vannt <nguyenvan261298@gmail.com>
2019-10-28 23:26:06 +0700

    thuát toán tìm kiếm số lớn nhất

---
+++ timKiemLonNhat.php
@@ -0,0 +1,15 @@
+<?php
+    function timKiemLonNhat($a = array()){
+        $max = 0;
+        $n = count($a);
+        for ($i = 0 ; $i<= $n+1 ;$i++){
+            if ($a[$i] >= $max){
+                $max = $a[$i];
+            }
+        }
+        return $max;
+    }
+$a = array(1, 2, 3, 44, 5, 6, 2, 33, 55, 78, 34, 6667, 8890, 45);
+    $max = timKiemLonNhat($a);
+    echo $max;
+?>
