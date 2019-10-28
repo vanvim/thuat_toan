8760cb3ff90d211b8826d9eba5fd0342f2dcca14
vannt <nguyenvan261298@gmail.com>
2019-10-29 00:20:04 +0700

    tính giai thua bằng thuật toán đệ quy

--- deQuyGiaiThua.php
+++ deQuyGiaiThua.php
@@ -1,1 +1,13 @@
 <?php
+function deQuyGiaiThua($n)
+{
+    if ($n <= 1) {
+        return $n;
+    } else {
+        return deQuyGiaiThua($n - 1) * $n;
+    }
+}
+
+$n = 9;
+$giaiThua = deQuyGiaiThua($n);
+echo 'Tổng của ' . $n . ' số liên tiếp là : ' . $giaiThua;
